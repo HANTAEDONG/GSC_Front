a6243e2beefa7fd48248dda5b9e2c065c74f8406
JunHyuk <oj041088@gmail.com>
2024-01-17 14:02:23 +0900

    second_1 commit (중복 호출 문제 있음)

--- src/components/Oauth.js
+++ src/components/Oauth.js
@@ -1,4 +1,4 @@
 const CLIENT_ID = "ecf7e443a56627d29bfcc53a5fa9e752";
-const REDIRECT_URI = "http://localhost:3000";
+const REDIRECT_URI = "http://localhost:3000/login/oauth2/callback/kakao";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
